532639a385b32856847b8d4d67b045cbb42008a9
HSUN.TSAI <hsuntsai0906@gmail.com>
2023-08-27 16:37:43 +0800

    style(Sidebar): handle overflow layout

--- src/app/components/sidebar/Sidebar.jsx
+++ src/app/components/sidebar/Sidebar.jsx
@@ -11,25 +11,31 @@ const Sidebar = () => {
 	const [collapsed, setCollapsed] = useState();
 
 	return (
-		<div className={classNames('sidebar', { 'sidebar--collapsed': collapsed })}>
-			<Menu
-				className="sidebar__menu"
-				mode="inline"
-				theme="dark"
-				inlineCollapsed={collapsed}
-				items={sideMenuSchema}
-			/>
-			<div className="sidebar__footer">
-				<div
-					className={classNames('sidebar__footer__version', {
-						'sidebar__footer__version--hide': collapsed,
-					})}
-				>
-					{`Version: v. ${process.env.VERSION}`}
+		<div className="sidebar">
+			<div className={classNames('sidebar__wrapper', { 'sidebar__wrapper--collapsed': collapsed })}>
+				<Menu
+					className="sidebar__wrapper__menu"
+					mode="inline"
+					theme="dark"
+					inlineCollapsed={collapsed}
+					items={sideMenuSchema}
+				/>
+				<div className="sidebar__wrapper__footer">
+					<div
+						className={classNames('sidebar__wrapper__footer__version', {
+							'sidebar__wrapper__footer__version--hide': collapsed,
+						})}
+					>
+						{`Version: v. ${process.env.VERSION}`}
+					</div>
+					<Button
+						className="sidebar__wrapper__footer__btn"
+						type="text"
+						onClick={() => setCollapsed(!collapsed)}
+					>
+						{collapsed ? <RightOutlined /> : <LeftOutlined />}
+					</Button>
 				</div>
-				<Button className="sidebar__footer__btn" type="text" onClick={() => setCollapsed(!collapsed)}>
-					{collapsed ? <RightOutlined /> : <LeftOutlined />}
-				</Button>
 			</div>
 		</div>
 	);
